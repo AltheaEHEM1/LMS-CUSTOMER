6db2920acecdbc7741fc09473c8e6d3c35834908
BenJr23 <rubenjrtbertuso@gmail.com>
2025-01-31 11:31:59 +0800

    pls

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/user/fetch', [RegisterController::class, 'getUser']);
 
 
 Route::get('/user/fetch', [RegisterController::class, 'getUser']);
