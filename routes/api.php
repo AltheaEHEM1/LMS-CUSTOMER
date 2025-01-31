0d443864f5ff1bbdc832fbebe47c31b5e2044efc
BenJr23 <rubenjrtbertuso@gmail.com>
2025-01-31 11:36:53 +0800

    gdfgdf

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::get('/fetch/user', [RegisterController::class, 'getUser']);
+Route::get('/user/fetch', [RegisterController::class, 'getUser']);
