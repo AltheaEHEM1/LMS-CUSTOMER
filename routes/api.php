4f68d6b36cc9c35b336bd81ad431af6a5fb466c3
BenJr23 <rubenjrtbertuso@gmail.com>
2025-01-31 11:22:35 +0800

    testing

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::get('/user/fetch', [RegistrationController::class, 'getUser']);
+Route::get('/user/fetch', [RegisterController::class, 'getUser']);
